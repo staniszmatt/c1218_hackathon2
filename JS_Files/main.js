9bd095b7951d9c010797460e3401ce1bbb1e4a6a
beeeweee <billdarnalljr@gmail.com>
2019-01-30 11:33:00 -0800

    maps location. Need to finish current location

--- JS_Files/main.js
+++ JS_Files/main.js
@@ -1,5 +1,7 @@
 $(document).ready(startWhenloaded)
 let initialization = null;
+var latt = 0;
+var long = 0;
 function startWhenloaded(){
   //TODO: Insert Click Handlers and editing of the DOM elements 
   let domInformation = {
@@ -10,4 +12,6 @@ function startWhenloaded(){
   }
   initialization = new PBGS_init(domInformation); //TODO: Move this and next line into the user interface class
   initialization.clickHandler();
+  testAjax();
+  returnData(response);
 }
