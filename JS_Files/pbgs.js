6776c05cd66a2de4acb0f798543dbec71b88f831
krispthan <44071872+krispthan@users.noreply.github.com>
2019-01-31 20:03:35 -0800

    Testing Setup

--- JS_Files/pbgs.js
+++ JS_Files/pbgs.js
@@ -6,31 +6,31 @@ class PBGS_init{
     this.domInformation = domInformation;
     this.upcScanner = new UPC_Scanner();
     this.barcodeInputValue = null;
-    // this.modalErrors = new Modal_error_message(); //TODO: Fix undefined
-    // this.modalErrors.hideModal();	
-    // this.modalErrors.show('barcode could not be read');
+    this.modalErrors = new Modal_error_message(); //TODO: Fix undefined
+    this.modalErrors.hideModal();	
+    this.modalErrors.show('barcode could not be read');
     
     //bindings
     this.clickHandler = this.clickHandler.bind(this);
     this.initScanner = this.initScanner.bind(this);
     this.submitBarcode = this.submitBarcode.bind(this);
     this.cancelScan = this.cancelScan.bind(this);
-    // this.whenCameraIsClick =this.whenCameraIsClick.bind(this);
-    // this.whenIndexClicked= this.whenIndexClicked.bind(this);
-    // this.whenGoogleClicked=this.whenGoogleClicked.bind(this);
-    // this.whenyouTubeClicked=this.whenyouTubeClicked.bind(this);
+    this.whenCameraIsClick =this.whenCameraIsClick.bind(this);
+    this.whenIndexClicked= this.whenIndexClicked.bind(this);
+    this.whenGoogleClicked=this.whenGoogleClicked.bind(this);
+    this.whenyouTubeClicked=this.whenyouTubeClicked.bind(this);
 
   }
 
   clickHandler(){
     this.domInformation.initScanButton.click(this.initScanner);
     this.domInformation.submitBarcodeNumberButton.click(this.submitBarcode);
-    // this.domInformation.modalButton.click(this.modalErrors.clickHandle);
+    this.domInformation.modalButton.click(this.modalErrors.clickHandle);
     this.domInformation.cancelScanButton.click(this.cancelScan);
-    // this.domInformation.whenCameraIsClick.click(this.whenCameraIsClick);
-    // this.domInformation.homeIcon.click(this.whenIndexClicked);
-    // this.domInformation.youtubeIcon.click(this.whenyouTubeClicked);
-    // this.domInformation.mapIcon.click(this.whenGoogleClicked);
+    this.domInformation.whenCameraIsClick.click(this.whenCameraIsClick);
+    this.domInformation.homeIcon.click(this.whenIndexClicked);
+    this.domInformation.youtubeIcon.click(this.whenyouTubeClicked);
+    this.domInformation.mapIcon.click(this.whenGoogleClicked);
 
   }
   initScanner(){ //calls when scan is submitted
@@ -42,8 +42,8 @@ class PBGS_init{
   submitBarcode(){ //calls with submitted by hand. 
     console.log("Clicked Submitt");//TODO: Go back to home screen if errors
     this.barcodeInputValue = this.domInformation.submitNumberInput.val();
-    //if the value of the barcode cannot be read, call modal
-    // this.modalErrors.show('barcode could not be read');
+    // if the value of the barcode cannot be read, call modal
+    this.modalErrors.show('barcode could not be read');
     this.upcScanner.processedCallBack(this.barcodeInputValue);
   }
   cancelScan(){
--- JS_Files/yt.js
+++ JS_Files/yt.js
@@ -1,64 +1,64 @@
-// $(document).ready(loadAndReady)
+$(document).ready(loadAndReady)
 
-// var key = 'AIzaSyARgDW5vI8D7LugaaKcd21ORq5ILiefBxM';
-// var playlistId = 'PL2fnLUTsNyq7A335zB_RpOzu7hEUcSJbB';
-// var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+var key = 'AIzaSyARgDW5vI8D7LugaaKcd21ORq5ILiefBxM';
+var playlistId = 'PL2fnLUTsNyq7A335zB_RpOzu7hEUcSJbB';
+var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
-// var options = {
-//     method: 'get',
-//     url: URL,
-//     dataType: 'json',
-//     data: {
-//         part: 'snippet',
-//         key: key,
-//         maxResults: 20,
-//         playlistId: playlistId
-//     },
-//     success: function (data) {
-//         var id = data.items[0].snippet.resourceId.videoId; // we need to find our data path on youtube
-//         mainVid(id);
-//         resultsLoop(data);
-//     }
-// }
+var options = {
+    method: 'get',
+    url: URL,
+    dataType: 'json',
+    data: {
+        part: 'snippet',
+        key: key,
+        maxResults: 20,
+        playlistId: playlistId
+    },
+    success: function (data) {
+        var id = data.items[0].snippet.resourceId.videoId; // we need to find our data path on youtube
+        mainVid(id);
+        resultsLoop(data);
+    }
+}
 
-// function loadAndReady() {
-//     // CLICK EVENT
-//     loadVids();
-//     $('main').on('click', 'article', function () {
-//         var id = $(this).attr('data-key');
-//         mainVid(id);
-//     });
+function loadAndReady() {
+    // CLICK EVENT
+    loadVids();
+    $('main').on('click', 'article', function () {
+        var id = $(this).attr('data-key');
+        mainVid(id);
+    });
 
-// }
-
-
-// function loadVids() {
-//     $.ajax(options);
-// }
-
-// function mainVid(id) {
-//     $('#video').html(
-//         // we will need to add in the correct source, this has to be dynamically done each time the user puts in their info, the 
-//         //source will change
-//         `<iframe width="350" height="250" id="youtube-video" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
-// }
+}
 
 
-// function resultsLoop(data) {
-//     $.each(data.items, function (i, item) {
-//         console.log("our data from the parameter", data);
-//         var thumb = item.snippet.thumbnails.medium.url;
-//         var title = item.snippet.title;
-//         var desc = item.snippet.description.substring(0, 100);
-//         var vid = item.snippet.resourceId.videoId;
-//         $('main').append(`
-//                               <article class="item" data-key="${vid}">
-//                                  <img src="${thumb}" alt="" class="thumb">
-//                                   <div class="details">
-//                                       <h4>${title}</h4>
-//                                       <p>${desc}</p>
-//                                   </div>
-//                               </article>
-//                           `);
-//     });
-// }
+function loadVids() {
+    $.ajax(options);
+}
+
+function mainVid(id) {
+    $('#video').html(
+        // we will need to add in the correct source, this has to be dynamically done each time the user puts in their info, the 
+        //source will change
+        `<iframe width="350" height="250" id="youtube-video" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+}
+
+
+function resultsLoop(data) {
+    $.each(data.items, function (i, item) {
+        console.log("our data from the parameter", data);
+        var thumb = item.snippet.thumbnails.medium.url;
+        var title = item.snippet.title;
+        var desc = item.snippet.description.substring(0, 100);
+        var vid = item.snippet.resourceId.videoId;
+        $('main').append(`
+                              <article class="item" data-key="${vid}">
+                                 <img src="${thumb}" alt="" class="thumb">
+                                  <div class="details">
+                                      <h4>${title}</h4>
+                                      <p>${desc}</p>
+                                  </div>
+                              </article>
+                          `);
+    });
+}
