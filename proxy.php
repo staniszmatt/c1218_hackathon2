18f729302c41e81c096f0071e5de78ee7332e058
staniszmatt <matthewpstaniszewski@gmail.com>
2019-04-09 15:36:38 -0700

    Updated Map Locations to OOP Updated maps to pull consistent location data Changed over to OOP Removed console.logs from all files Updated Tabing and spacing on all files

--- proxy.php
+++ proxy.php
@@ -12,22 +12,22 @@ curl_setopt($ch, CURLOPT_HEADER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//  "user_key: $user_key",
-//  "key_type: 3scale"
+  //  "user_key: $user_key",
+  //  "key_type: 3scale"
 ));
 
 // HTTP GET
 curl_setopt($ch, CURLOPT_POST, 0);
-curl_setopt($ch, CURLOPT_URL, $endpoint.'?upc='.$_GET['upc']);
+curl_setopt($ch, CURLOPT_URL, $endpoint . '?upc=' . $_GET['upc']);
 $response = curl_exec($ch);
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 if ($httpcode != 200) {
-    echo "error status $httpcode...\n";
+  echo "error status $httpcode...\n";
 } else {
-    $bodyStart = strpos($response, "\r\n\r\n");
+  $bodyStart = strpos($response, "\r\n\r\n");
 }
-        $body = substr($response, $bodyStart + 4);
- echo $body."\n";
+$body = substr($response, $bodyStart + 4);
+echo $body . "\n";
 /* if you need to run more queries, do them in the same connection.
  * use rawurlencode() instead of URLEncode(), if you set search string
  * as url query param
